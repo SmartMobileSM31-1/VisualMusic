9c3099a5f21a35d580e12372522ca7adc45b4422
Bob Steers <kargathia@hotmail.com>
2014-10-12 16:47:48 +0200

    Initial Commit

---
+++ VisualMusic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VisualMusic
+//
+//  Created by Fhict on 12/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
